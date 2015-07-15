5a22264c72ac766a7da142ba7a39c101eb945c5a
PavelVlasenko <vlasenko-pav@yandex.ru>
2015-07-15 17:44:30 +0700

    CheckFilePermissions Load policy file

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        System.out.println("===== Start security tool");
+        SecurityManagerTool securityManagerTool = new SecurityManagerTool();
+
+        securityManagerTool.loadPolicyFile("/home/pvlasenko/Documents/Upwork/SecurityManager/security.policy");
+
+        //test
+        System.out.println("Unsafe operation test");
+        securityManagerTool.checkOpenFile("/home/pvlasenko/test/testFileUnsafe.txt");
+
+        System.out.println("Safe operation");
+        securityManagerTool.checkOpenFile("/home/pvlasenko/test/testFileSafe.txt");
     }
 }
---
+++ src/SecurityManagerTool.java
@@ -0,0 +1,40 @@
+import java.security.AccessController;
+
+/**
+ * TODO: Java docs
+ *
+ * @author Pavel Vlasenko
+ */
+public class SecurityManagerTool
+{
+    SecurityManager security;
+
+    public void loadPolicyFile(String filePath)
+    {
+        System.setProperty("java.security.policy","file:"+filePath);
+        security = new SecurityManager();
+        System.setSecurityManager(security);
+    }
+
+    public void checkOpenFile(String filePath)
+    {
+       // SecurityManager security = System.getSecurityManager();
+        try
+        {
+            if(security != null)
+            {
+                security.getSecurityContext();
+                security.checkRead(filePath);
+            }
+            //if safe do this action
+            System.out.println("Safe operation");
+        }
+        catch(SecurityException e)
+        {
+            //if unsafe do this action
+            System.out.println("Unsafe operation");
+        }
+    }
+
+
+}
