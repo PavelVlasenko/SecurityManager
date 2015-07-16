2a072ed9d5f310d14b92a1622689b7697e179248
PavelVlasenko <vlasenko-pav@yandex.ru>
2015-07-16 15:39:38 +0700

    CheckFilePermissions Load policy file

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.Enumeration;
+
 public class Main {
 
     public static void main(String[] args)
     {
+         testCert();
+    }
+
+
+    public void testPolicy()
+    {
         System.out.println("===== Start security tool");
         SecurityManagerTool securityManagerTool = new SecurityManagerTool();
 
-        securityManagerTool.loadPolicyFile("/home/pvlasenko/Documents/Upwork/SecurityManager/security.policy");
+        securityManagerTool.loadPolicyFile("/home/pvlasenko/Documents/Upwork/SecurityManager/security.config");
 
         //test
         System.out.println("Unsafe operation test");
@@ -13,5 +30,56 @@ public class Main {
 
         System.out.println("Safe operation");
         securityManagerTool.checkOpenFile("/home/pvlasenko/test/testFileSafe.txt");
+    }
+
+    public static void testCert()
+    {
+        FileInputStream is = null;
+        try {
+
+            File file = new File("/home/pvlasenko/security.jks");
+            is = new FileInputStream(file);
+            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+            String password = "123123";
+            keystore.load(is, password.toCharArray());
+
+
+//            Enumeration enumeration = keystore.aliases();
+//            while(enumeration.hasMoreElements()) {
+//                String alias = (String)enumeration.nextElement();
+//                System.out.println("alias name: " + alias);
+//                Certificate certificate = keystore.getCertificate(alias);
+//                System.out.println(certificate.toString());
+//            }
+
+            String allias = "bob";
+            Certificate c = keystore.getCertificate(allias);
+
+            CertificateFactory fact = CertificateFactory.getInstance("X.509");
+            FileInputStream is2 = new FileInputStream ("/home/pvlasenko/bob.crt");
+            Certificate cer =  fact.generateCertificate(is2);
+            boolean compare = c.equals(cer);
+
+
+
+        } catch (java.security.cert.CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(null != is)
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+        }
     }
 }
--- src/SecurityManagerTool.java
+++ src/SecurityManagerTool.java
@@ -37,4 +37,7 @@ public class SecurityManagerTool
     }
 
 
+    //check Sertification
+
+
 }
